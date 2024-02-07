6c678a6262aea55ab5f24fab6705cd25ff667c87
Mohamed Nheri <nherimohamed@gmail.com>
2024-02-06 20:18:06 -0500

    Initial commit

---
+++ src/session1/TestNGDemo.java
@@ -0,0 +1,26 @@
+package session1;
+
+
+import org.testng.annotations.Test;
+
+public class TestNGDemo {
+
+    //IDE Eclipse  -- You have to download a testng plugin
+    //IDE IntellJ -- Does have the plugin out of the box
+
+    @Test
+    void greeting(){
+        System.out.println("Good morning");
+    }
+
+    @Test
+    void greeting2(){
+        System.out.println("Good morning");
+    }
+
+    @Test
+    void greeting3(){
+        System.out.println("Good morning");
+    }
+
+}
